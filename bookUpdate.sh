1d4e7c08c98ef9644a2bf738d9dc867f8126e4da
JavaliMZ <syjulio123@gmail.com>
2021-09-10 14:02:44 +0100

    testing bookUpdate.sh

--- bookUpdate.sh
+++ bookUpdate.sh
@@ -16,21 +16,3 @@ git branch -m main
 # NOTE: you might have to setup you GitHub access token
 git remote add github https://github.com/JavaliMZ/JavaliMZ.github.io
 git push github main
-# deploy the static site to GitHub Pages
-git checkout --orphan gh-pages
-git rm -rf .
-rm '.gitignore'
-
-git commit --allow-empty -m "new clean branch"
-git checkout main
-git worktree add /tmp/book gh-pages
-git worktree list
-mdbook build
-rm -rf /tmp/book/*
-cp -rp book/* /tmp/book/
-pushd /tmp/book
-git add -A
-git commit -m 'new blog deployed'
-git push github gh-pages
-popd
-git worktree remove /tmp/book
